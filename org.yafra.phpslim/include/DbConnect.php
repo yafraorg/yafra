d1eb0b887599d7420f19189c560ff881630b2355
yafraorg <yafraorg@gmail.com>
2014-05-22 00:13:48 +0200

    updated e4 and php5 dbo

--- org.yafra.phpslim/include/DbConnect.php
+++ org.yafra.phpslim/include/DbConnect.php
@@ -21,7 +21,8 @@ class DbConnect {
         include_once dirname(__FILE__) . '/Config.php';
 
         // Connecting to mysql database
-        $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        //$this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        $db = new PDO('mysql:host=localhost;dbname=yafra;charset=utf8', DB_USERNAME, DB_PASSWORD);
 
         // Check for database connection error
         if (mysqli_connect_errno()) {
