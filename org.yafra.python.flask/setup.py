c44a4d16bcec44ba299d975a33a74567a0ff25e6
Yafra <yafraorg@gmail.com>
2014-06-08 21:55:52 +0200

    updated python flask to Pillow

--- org.yafra.python.flask/setup.py
+++ org.yafra.python.flask/setup.py
@@ -8,5 +8,5 @@ setup(name='YafraPython',
       author_email='yafraorg@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Flask>=0.10', 'xlwt>=0.7.5', 'reportlab>=3.0', 'MarkupSafe>=0.18', 'Werkzeug>=0.9.4',
-                        'Flask-SQLAlchemy>=1.0', 'Jinja2>=2.7.2', 'SQLAlchemy>=0.9.3', 'PIL>=1.1.7'],
+                        'Flask-SQLAlchemy>=1.0', 'Jinja2>=2.7.2', 'SQLAlchemy>=0.9.3', 'Pillow>=2.4.0'],
 )
