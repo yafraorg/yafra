95dab54775be0425b8a7494b1abde418e887b94a
Yafra <yafraorg@gmail.com>
2014-06-09 22:50:47 +0200

    updated flask

---
+++ org.yafra.python.flask/wsgi/app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.mail import Mail
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+from config import config
+
+__author__ = 'mwn'
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    # attach routes and custom error pages here
+
+    return app
+
