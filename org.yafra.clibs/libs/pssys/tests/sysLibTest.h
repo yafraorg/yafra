482b4b5041ad4835bcb4ce3665a4e6aff79c7c92
yafraorg <yafraorg@gmail.com>
2014-12-08 13:52:44 +0100

    added skeleton for cppunit

---
+++ org.yafra.clibs/libs/pssys/tests/sysLibTest.h
@@ -0,0 +1,21 @@
+/*
+ * characterTest.h
+ *
+ *  Created on: Dec 3, 2014
+ *      Author: mwn
+ */
+
+#ifndef LIBS_PSSYS_TESTS_SYSLIBTEST_H_
+#define LIBS_PSSYS_TESTS_SYSLIBTEST_H_
+
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+extern "C" {
+#include <pssys.h>
+}
+
+#endif /* LIBS_PSSYS_TESTS_SYSLIBTEST_H_ */
