2322698c6f532b8703387bbdbcea28b2f7682f42
Yafra <yafraorg@gmail.com>
2015-02-22 00:07:46 +0100

    changed port

--- org.yafra.sysadm/shellscripts/run-yafra-java.sh
+++ org.yafra.sysadm/shellscripts/run-yafra-java.sh
@@ -1,2 +1,2 @@
-#docker run -d -p 8080:8080 --name yafrajava yafraorg/docker-yafrajava
-docker run -t -p 8080:8080 --name yafrajava -i yafraorg/docker-yafrajava /bin/bash
+#docker run -d -p 8081:8081 --name yafrajava yafraorg/docker-yafrajava
+docker run -t -p 8081:8081 --name yafrajava -i yafraorg/docker-yafrajava /bin/bash
