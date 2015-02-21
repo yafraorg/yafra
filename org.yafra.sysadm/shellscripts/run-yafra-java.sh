98b735fb5a8be1c4adace67df29ce712873d5359
Yafra <yafraorg@gmail.com>
2015-02-22 00:44:25 +0100

    linking on java docker

--- org.yafra.sysadm/shellscripts/run-yafra-java.sh
+++ org.yafra.sysadm/shellscripts/run-yafra-java.sh
@@ -1,2 +1,4 @@
 #docker run -d -p 8081:8081 --name yafrajava yafraorg/docker-yafrajava
-docker run -t -p 8081:8081 --name yafrajava -i yafraorg/docker-yafrajava /bin/bash
+#docker run -t -p 8081:8081 --name yafrajava -i yafraorg/docker-yafrajava /bin/bash
+#assuming yaframysql runs already
+docker run -P --name java --link yaframysql:yaframysql -i -t yafraorg/docker-yafrajava /bin/bash
