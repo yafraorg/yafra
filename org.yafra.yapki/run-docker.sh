e410874c594cbd931b491e9615a7c3e612f403e5
Yafra <yafraorg@gmail.com>
2015-03-01 00:56:16 +0100

    updated docker run script

--- org.yafra.yapki/run-docker.sh
+++ org.yafra.yapki/run-docker.sh
@@ -6,14 +6,15 @@
 # setup local environment first https://github.com/yafraorg/yafra/wiki/Development-Environment
 export BASENODE=/work/repos/yafra
 export YAPKI=$BASENODE/org.yafra.yapki
-export WWWDIR=/usr/share/nginx/html
+export WWWDIR=/var/www/html
 export WORKNODE=/work/yafra-runtime
 test -d $WORKNODE/bin || mkdir -p $WORKNODE/bin
 
 echo "download latest build and make it available as nginx webapp"
 cd $YAPKI
-ll
-
-
+git pull
+cp cgi/*.pl /usr/lib/cgi-bin/
+cd www
+cp -r * $WWWDIR
 
 echo "done - running now YAPKI under nginx/perl/python with admin scripts under /usr/local/bin"
