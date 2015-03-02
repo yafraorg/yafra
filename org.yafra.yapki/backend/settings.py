2ce561fc5b4dd8bf78d5f5c322560db5843804f0
yafraorg <yafraorg@gmail.com>
2015-03-02 14:31:58 +0100

    added mongo db server settings

--- org.yafra.yapki/backend/settings.py
+++ org.yafra.yapki/backend/settings.py
@@ -1,1 +1,11 @@
+# Let's just use the local mongod instance. Edit as needed.
+
+# Please note that MONGO_HOST and MONGO_PORT could very well be left
+# out as they already default to a bare bones local 'mongod' instance.
+MONGO_HOST = os.environ.get('DB_PORT_27017_TCP_ADDR')
+MONGO_PORT = 27017
+MONGO_USERNAME = 'yafraadmin'
+MONGO_PASSWORD = 'yafra'
+MONGO_DBNAME = 'yapki'
+
 DOMAIN = {'people': {}}
