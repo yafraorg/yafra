ca7f9d143e4e88a1af356744eb7fe7dbf8856255
yafraorg <yafraorg@gmail.com>
2015-03-02 14:54:25 +0100

    updated eve app

--- org.yafra.yapki/backend/run.py
+++ org.yafra.yapki/backend/run.py
@@ -1,6 +1,47 @@
+#-------------------------------------------------------------------------------
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# (c) yafra.org, 2002, Switzerland
+#
+#-------------------------------------------------------------------------------
+#
+# python eve run.py
+
+import os
 from eve import Eve
+
+# Heroku support: bind to PORT if defined, otherwise default to 5000.
+if 'PORT' in os.environ:
+    port = int(os.environ.get('PORT'))
+    # use '0.0.0.0' to ensure your REST API is reachable from all your
+    # network (and not only your computer).
+    host = '0.0.0.0'
+else:
+    port = 8080
+    host = '127.0.0.1'
+
 app = Eve()
 
-if __name__ == '__main__':
-    app.run()
 
+@app.after_request
+def after_request(response):
+    response.headers.add('X-Test', 'This is only test.')
+    return response
+
+if __name__ == '__main__':
+    app.run(host=host, port=port)
--- org.yafra.yapki/backend/settings.py
+++ org.yafra.yapki/backend/settings.py
@@ -1,11 +1,140 @@
-# Let's just use the local mongod instance. Edit as needed.
+#-------------------------------------------------------------------------------
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# (c) yafra.org, 2002, Switzerland
+#
+#-------------------------------------------------------------------------------
+#
+# python eve settings.py
+
+import os
 
 # Please note that MONGO_HOST and MONGO_PORT could very well be left
 # out as they already default to a bare bones local 'mongod' instance.
-MONGO_HOST = os.environ.get('DB_PORT_27017_TCP_ADDR')
+MONGO_HOST = os.environ.get('DB_PORT_27017_TCP_ADDR', 'localhost')
 MONGO_PORT = 27017
 MONGO_USERNAME = 'yafraadmin'
 MONGO_PASSWORD = 'yafra'
 MONGO_DBNAME = 'yapki'
 
-DOMAIN = {'people': {}}
+
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+# Enable reads (GET), edits (PATCH) and deletes of individual items
+# (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'DELETE']
+
+# We enable standard client cache directives for all resources exposed by the
+# API. We can always override these global settings later.
+CACHE_CONTROL = 'max-age=20'
+CACHE_EXPIRES = 20
+
+# Our API will expose two resources (MongoDB collections): 'people' and
+# 'works'. In order to allow for proper data validation, we define beaviour
+# and structure.
+people = {
+    # 'title' tag used in item links.
+    'item_title': 'person',
+
+    # by default the standard item entry point is defined as
+    # '/people/<ObjectId>/'. We leave it untouched, and we also enable an
+    # additional read-only entry point. This way consumers can also perform GET
+    # requests at '/people/<lastname>/'.
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'lastname'
+    },
+
+    # Schema definition, based on Cerberus grammar. Check the Cerberus project
+    # (https://github.com/nicolaiarocci/cerberus) for details.
+    'schema': {
+        'firstname': {
+            'type': 'string',
+            'minlength': 1,
+            'maxlength': 10,
+        },
+        'lastname': {
+            'type': 'string',
+            'minlength': 1,
+            'maxlength': 15,
+            'required': True,
+            # talk about hard constraints! For the purpose of the demo
+            # 'lastname' is an API entry-point, so we need it to be unique.
+            'unique': True,
+        },
+        # 'role' is a list, and can only contain values from 'allowed'.
+        'role': {
+            'type': 'list',
+            'allowed': ["author", "contributor", "copy"],
+        },
+        # An embedded 'strongly-typed' dictionary.
+        'location': {
+            'type': 'dict',
+            'schema': {
+                'address': {'type': 'string'},
+                'city': {'type': 'string'}
+            },
+        },
+        'born': {
+            'type': 'datetime',
+        },
+    }
+}
+
+works = {
+    # if 'item_title' is not provided Eve will just strip the final
+    # 's' from resource name, and use it as the item_title.
+    #'item_title': 'work',
+
+    # We choose to override global cache-control directives for this resource.
+    'cache_control': 'max-age=10,must-revalidate',
+    'cache_expires': 10,
+
+    'schema': {
+        'title': {
+            'type': 'string',
+            'required': True,
+        },
+        'description': {
+            'type': 'string',
+        },
+        'owner': {
+            'type': 'objectid',
+            'required': True,
+            # referential integrity constraint: value must exist in the
+            # 'people' collection. Since we aren't declaring a 'field' key,
+            # will default to `people._id` (or, more precisely, to whatever
+            # ID_FIELD value is).
+            'data_relation': {
+                'resource': 'people',
+                # make the owner embeddable with ?embedded={"owner":1}
+                'embeddable': True
+            },
+        },
+    }
+}
+
+# The DOMAIN dict explains which resources will be available and how they will
+# be accessible to the API consumer.
+DOMAIN = {
+    'people': people,
+    'works': works,
+}
