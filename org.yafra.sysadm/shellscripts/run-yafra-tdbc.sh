906a5c56192e83d4622472587279dcd2e9d501bb
Yafra Owner <yafraorg@gmail.com>
2015-03-23 21:51:23 +0100

    fixed ports

--- org.yafra.sysadm/shellscripts/run-yafra-tdbc.sh
+++ org.yafra.sysadm/shellscripts/run-yafra-tdbc.sh
@@ -1,4 +1,4 @@
 #assuming yaframysql runs already
 #docker start yaframysql
-#docker run -d -p 8081:8081 --name yafrac --link yaframysql:db yafraorg/docker-yafrac
-docker run --rm -P --name yafrac --link yaframysql:db -i -t yafraorg/docker-yafrac /bin/bash
+#docker run -d -p 5000:5000 -p 5001:5001 -p 5002:5002 --name yafrac --link yaframysql:db yafraorg/docker-yafrac
+docker run --rm -p 5000:5000 -p 5001:5001 -p 5002:5002 --name yafrac --link yaframysql:db -i -t yafraorg/docker-yafrac /bin/bash
--- org.yafra.sysadm/shellscripts/run-yafra-tdbnet.sh
+++ org.yafra.sysadm/shellscripts/run-yafra-tdbnet.sh
@@ -1,4 +1,4 @@
 #assuming yaframysql runs already
 #docker start yaframysql
-#docker run -d -p 8081:8081 --name yafranet --link yaframysql:db yafraorg/docker-yafranet
-docker run --rm -P --name yafranet --link yaframysql:db -i -t yafraorg/docker-yafranet /bin/bash
+#docker run -d --name yafranet --link yaframysql:db yafraorg/docker-yafranet
+docker run --rm --name yafranet --link yaframysql:db -i -t yafraorg/docker-yafranet /bin/bash
