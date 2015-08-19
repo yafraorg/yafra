0237e5a5487fee33b2b6bb1da91041719711188b
Yafra Admin <yafraorg@gmail.com>
2015-08-19 21:52:38 +0200

    add relevant ports including udp

--- org.yafra.sysadm/shellscripts/run-yafra-tv.sh
+++ org.yafra.sysadm/shellscripts/run-yafra-tv.sh
@@ -1,3 +1,3 @@
 # run the SageTV server
 
-docker run -P -t -i --rm --name yafratv yafraorg/docker-yafratv /bin/bash
+docker run -p 80:80 -p 7818:7818 -p 8080:8080 -p 8270:8270/udp -p 31100:31100/udp -p 31099:31099 -p 42024:42024 -t -i --rm --name yafratv yafraorg/docker-yafratv /bin/bash
