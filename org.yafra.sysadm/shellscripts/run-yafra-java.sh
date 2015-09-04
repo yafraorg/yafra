ea5e6f4aa0da98779459c085b86336ac08cd9f96
Yafra Admin <yafraorg@gmail.com>
2015-09-04 19:37:49 +0200

    converted start scripts to start as deamon

--- org.yafra.sysadm/shellscripts/run-yafra-java.sh
+++ org.yafra.sysadm/shellscripts/run-yafra-java.sh
@@ -1,4 +1,4 @@
 #assuming yaframysql runs already
 #docker start yaframysql
-#docker run -d -p 8080:8080 -p 4201:4201 --name yafrajava --link yaframysql:db yafraorg/docker-yafrajava
-docker run --rm -p 8080:8080 -p 4201:4201 --name yafrajava --link yaframysql:db -i -t yafraorg/docker-yafrajava /bin/bash
+docker run -d -p 8080:8080 -p 4201:4201 --name yafrajava --link yaframysql:db yafraorg/docker-yafrajava
+#docker run --rm -p 8080:8080 -p 4201:4201 --name yafrajava --link yaframysql:db -i -t yafraorg/docker-yafrajava /bin/bash
