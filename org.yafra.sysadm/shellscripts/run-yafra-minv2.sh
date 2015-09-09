f8a635bb66c7975e68dc6bed5bf93d6c4ef3d0cd
Yafra Admin <yafraorg@gmail.com>
2015-09-09 10:13:10 +0200

    change the docker scripts to be interactice as currently they are mainly used to debug

--- org.yafra.sysadm/shellscripts/run-yafra-minv2.sh
+++ org.yafra.sysadm/shellscripts/run-yafra-minv2.sh
@@ -1,2 +1,1 @@
-#docker run -d -p 3306:3306 --name yaframysql yafraorg/docker-yafradb:mysql
-docker run -t --name minv2 -i shippable/minv2:beta /bin/bash
+docker run --rm -t --name minv2 -i shippable/minv2:latest /bin/bash
--- org.yafra.sysadm/shellscripts/run-yafra-tv.sh
+++ org.yafra.sysadm/shellscripts/run-yafra-tv.sh
@@ -1,7 +1,7 @@
 # run the SageTV server
 
-docker run -d -P --net=host --name yafratv yafraorg/docker-yafratv
+#docker run -d -P --net=host --name yafratv yafraorg/docker-yafratv
 
 #docker run -p 80:80 -p 7818:7818 -p 8080:8080 -p 8270:8270/udp -p 31100:31100/udp -p 31099:31099 -p 42024:42024 --net=host -t -i --rm --name yafratv yafraorg/docker-yafratv /bin/bash
-#docker run -P --net=host -t -i --rm --name yafratv yafraorg/docker-yafratv /bin/bash
+docker run -P --net=host -t -i --rm --name yafratv yafraorg/docker-yafratv /bin/bash
 #docker exec -it yafratv /bin/bash
