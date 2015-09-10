9570d2cdf6329dd07651bee76d42557a6b03cb72
Yafra Admin <yafraorg@gmail.com>
2015-09-10 15:20:40 +0200

    changed ports on yapki

--- org.yafra.sysadm/shellscripts/run-yafra-pki.sh
+++ org.yafra.sysadm/shellscripts/run-yafra-pki.sh
@@ -2,5 +2,6 @@
 #./run-yafra-mongodb.sh
 
 # run the pki with mongo and dataonly links/volumes
-#docker run -p 80:80  -p 8080:8080 -d --volumes-from yafraorg/docker-yafradb:dataonly --link yaframongo:db --name yapki yafraorg/docker-yapki
-docker run -p 80:80 -p 8080:8080 -t -i --rm --volumes-from yafradata --link yaframongo:db --name yapki yafraorg/docker-yapki /bin/bash
+docker run -p 8081:8081  -p 8082:8082 -d --volumes-from yafradata --link yaframongo:db --name yapki yafraorg/docker-yapki
+#docker run -p 8081:8081 -p 8082:8082 -t -i --rm --volumes-from yafradata --link yaframongo:db --name yapki yafraorg/docker-yapki /bin/bash
+#docker exec -it yapki /bin/bash
