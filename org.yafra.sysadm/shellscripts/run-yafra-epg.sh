9a20173bc9fa1085c466dff95ce63cb836e6a7e1
Yafra <yafraorg@yafra.org>
2016-06-04 21:17:06 +0200

    updated docker egp start script

--- org.yafra.sysadm/shellscripts/run-yafra-epg.sh
+++ org.yafra.sysadm/shellscripts/run-yafra-epg.sh
@@ -1,4 +1,5 @@
 # run the EPG XMLTV grabber 
 
 # run the pki with mongo and dataonly links/volumes
-docker run -t -i --rm --entrypoint=/bin/bash --name yafraepg yafraorg/docker-yafraepg
+docker run -t -i --rm -v /work:/opt/egp --entrypoint=/bin/bash --name yafraepg yafraorg/docker-yafraepg
+#docker run -t -i --rm -v /work:/opt/egp --name yafraepg yafraorg/docker-yafraepg
